f39a62d5bcadcacb054839e3d6960712f7058f5b
ArturMandryk <ArturMandryk@gmail.com>
2022-09-23 09:49:41 +0300

    11

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,11 +5,7 @@ const getGalleryRef = document.querySelector('.gallery');
 const listGalleryMarkup = galleryItems
   .map(
     ({ preview, description, original }) =>
-<<<<<<< HEAD
-      /*html*/ ` <a class='gallery__item' href='${original}' ><img class='gallery__image' src='${preview}' alt='${description}' > </a> `
-=======
-      /*html*/ ` <li><a class='gallery__item' href='${original}' ><img class='gallery__image' src='${preview}' alt='${description}' title="${description}"> </a></li> `
->>>>>>> 2b51e6844d76c8ffefb457086553b8b328a9e2b2
+      ` <li><a class='gallery__item' href='${original}' ><img class='gallery__image' src='${preview}' alt='${description}' title="${description}"> </a></li> `
   )
   .join('');
 
